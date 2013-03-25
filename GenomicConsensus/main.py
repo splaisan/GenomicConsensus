7d9cfe955c9b4b66866117863e26b2e03e5d1cc8
David Alexander <dalexander@pacificbiosciences.com>
2013-03-25 11:50:54 -0800

    Go back to "cumulative" time profiling
    [Tracking bug 22690]
    
    [git-p4: depot-paths = "//depot/software/bioinformatics/tools/GenomicConsensus/": change = 121777]

--- GenomicConsensus/main.py
+++ GenomicConsensus/main.py
@@ -183,7 +183,7 @@ class ToolRunner(object):
 
     def _printProfiles(self):
         for profile in glob.glob(os.path.join(options.temporaryDirectory, "*")):
-            pstats.Stats(profile).sort_stats("time").print_stats(20)
+            pstats.Stats(profile).sort_stats("cumulative").print_stats(20)
 
     def _cleanup(self):
         if options.doProfiling:
