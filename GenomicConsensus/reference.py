91f446163be24ea668abaf6f9e2a57242bfa0131
Martin D. Smith <mdsmith@pacificbiosciences.com>
2015-07-03 20:25:02 -0800

    [Bug 26596] numRecords and totalLength filled and correct (tested with bam+pbi files so far, filtered or not). Revamped refId system throughout (and in relation to quiver).
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/tools/GenomicConsensus/": change = 153854]

--- GenomicConsensus/reference.py
+++ GenomicConsensus/reference.py
@@ -127,7 +127,13 @@ def loadFromFile(filename_, cmpH5):
     except IOError as e:
         die(e)
 
-    cmpContigNames = set(cmpH5.refNames)
+    if cmpH5.isCmpH5:
+        # This shouldn't have to be the case, but there are some underlying
+        # issues here with naming across fasta, cmp, and bam files. id,
+        # shortname, name, and fullname. MDS will figure out and document.
+        cmpContigNames = set(cmpH5.fullRefNames)
+    else:
+        cmpContigNames = set(cmpH5.refNames)
 
     for fastaRecord in f.contigs:
         refName = fastaRecord.id
