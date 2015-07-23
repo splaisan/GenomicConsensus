8222d7cdc5b0653c34f4d8c4a050a7812521f521
Nathaniel Echols <nechols@pacb.com>
2015-07-23 11:31:30 -0800

    clean up TCI-related tests; bug 26968
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/tools/GenomicConsensus/": change = 155578]

--- tests/unit/test_tool_contract.py
+++ tests/unit/test_tool_contract.py
@@ -1,6 +1,4 @@
 
-# TODO add quiver test (replacing cram test)
-
 import unittest
 import os.path
 
@@ -12,6 +10,24 @@ assert os.path.isdir(DATA_DIR)
 
 # optional (but required for TestSummarizeConsensus)
 DATA_DIR_2 = "/mnt/secondary/Share/Quiver/TestData/tinyLambda/"
+
+class TestQuiver(pbcommand.testkit.PbTestApp):
+    DRIVER_BASE = "variantCaller "
+    DRIVER_EMIT = DRIVER_BASE + " --emit-tool-contract "
+    DRIVER_RESOLVE = DRIVER_BASE + " --resolved-tool-contract "
+    REQUIRES_PBCORE = True
+    INPUT_FILES = [
+        os.path.join(DATA_DIR, "hcv", "aligned_reads.cmp.h5"),
+        os.path.join(DATA_DIR, "hcv", "HCV_Ref_For_187140.fasta"),
+    ]
+    TASK_OPTIONS = {
+      "genomic_consensus.task_options.min_coverage": 0,
+      "genomic_consensus.task_options.min_confidence": 0,
+      "genomic_consensus.task_options.algorithm": "quiver",
+      "genomic_consensus.task_options.diploid": False,
+      "genomic_consensus.task_options.parameter_spec": "unknown"
+    }
+
 
 class TestGffToBed(pbcommand.testkit.PbTestApp):
     DRIVER_BASE = "gffToBed "
@@ -28,6 +44,7 @@ class TestGffToBed(pbcommand.testkit.PbTestApp):
         "genomic_consensus.task_options.use_score": 0,
     }
 
+
 class TestGffToVcf(pbcommand.testkit.PbTestApp):
     DRIVER_BASE = "gffToVcf"
     DRIVER_EMIT = DRIVER_BASE + " --emit-tool-contract "
@@ -39,6 +56,7 @@ class TestGffToVcf(pbcommand.testkit.PbTestApp):
     TASK_OPTIONS = {
         "genomic_consensus.task_options.global_reference": "Staphylococcus_aureus_USA300_TCH1516",
     }
+
 
 @unittest.skipUnless(os.path.isdir(DATA_DIR_2), "Missing %s" % DATA_DIR_2)
 class TestSummarizeConsensus(pbcommand.testkit.PbTestApp):
@@ -55,6 +73,7 @@ class TestSummarizeConsensus(pbcommand.testkit.PbTestApp):
     def run_after(self, rtc, output_dir):
         # FIXME using default file name
         self.assertTrue(os.path.isfile(os.path.join(output_dir, "file.gff")))
+
 
 if __name__ == "__main__":
     unittest.main()
