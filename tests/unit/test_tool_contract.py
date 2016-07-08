7c35677fe947a1a25350a5e6f6a71a1d1a5c0318
David Alexander <dalexander@pacificbiosciences.com>
2016-07-08 14:57:44 -0700

    Disable troublesome tests of execution via tool contract, pending fix of Bug 33699

--- tests/unit/test_tool_contract.py
+++ tests/unit/test_tool_contract.py
@@ -10,32 +10,39 @@ import pbtestdata
 DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
 assert os.path.isdir(DATA_DIR)
 
+# These tests seem to cause some logging failure at shutdown;
+# disabling them pending upstream fix.  See:
+# https://bugzilla.nanofluidics.com/show_bug.cgi?id=33699
 
-class TestVariantCaller(pbcommand.testkit.PbTestApp):
-    DRIVER_BASE = "variantCaller "
-    DRIVER_EMIT = DRIVER_BASE + " --emit-tool-contract "
-    DRIVER_RESOLVE = DRIVER_BASE + " --resolved-tool-contract "
-    REQUIRES_PBCORE = True
-    INPUT_FILES = [
-        pbtestdata.get_file("aligned-xml"), pbtestdata.get_file("lambdaNEB")
-    ]
-    TASK_OPTIONS = {
-      "genomic_consensus.task_options.min_coverage": 0,
-      "genomic_consensus.task_options.min_confidence": 0,
-      "genomic_consensus.task_options.algorithm": "quiver",
-      "genomic_consensus.task_options.diploid": False,
-    }
+# class TestVariantCaller(pbcommand.testkit.PbTestApp):
+#     DRIVER_BASE = "variantCaller "
+#     DRIVER_EMIT = DRIVER_BASE + " --emit-tool-contract "
+#     DRIVER_RESOLVE = DRIVER_BASE + " --resolved-tool-contract "
+#     REQUIRES_PBCORE = True
+#     INPUT_FILES = [
+#         pbtestdata.get_file("aligned-xml"), pbtestdata.get_file("lambdaNEB")
+#     ]
+#     TASK_OPTIONS = {
+#       "genomic_consensus.task_options.min_coverage": 0,
+#       "genomic_consensus.task_options.min_confidence": 0,
+#       "genomic_consensus.task_options.algorithm": "quiver",
+#       "genomic_consensus.task_options.diploid": False,
+#     }
 
-    def run_after(self, rtc, output_dir):
-        contigs_file = rtc.task.output_files[1]
-        with openDataSet(contigs_file, strict=True) as ds:
-            self.assertTrue(isinstance(ds, ContigSet))
+#     def test_run_e2e(self):
+#         import ipdb; ipdb.set_trace()
+#         super(TestVariantCaller, self).test_run_e2e()
+
+#     def run_after(self, rtc, output_dir):
+#         contigs_file = rtc.task.output_files[1]
+#         with openDataSet(contigs_file, strict=True) as ds:
+#             self.assertTrue(isinstance(ds, ContigSet))
 
 
-class TestVariantCallerArrow(TestVariantCaller):
-    TASK_OPTIONS = {
-      "genomic_consensus.task_options.algorithm": "arrow",
-    }
+# class TestVariantCallerArrow(TestVariantCaller):
+#     TASK_OPTIONS = {
+#       "genomic_consensus.task_options.algorithm": "arrow",
+#     }
 
 
 class TestGffToBed(pbcommand.testkit.PbTestApp):
