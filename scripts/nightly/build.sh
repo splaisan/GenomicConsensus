1e70c7b29b99637d405665be872b4df3c437edb1
David Alexander <dalexander@pacificbiosciences.com>
2016-10-20 09:30:24 -0700

    The nightly build script needs the workaround for the "empty git remote" problem, too

--- scripts/nightly/build.sh
+++ scripts/nightly/build.sh
@@ -42,7 +42,12 @@ then
 fi
 
 echo "## Fetch unanimity submodules"
-( cd _deps/unanimity && git submodule update --init --remote )
+# Bamboo's checkout of unanimity doesn't set the "origin" remote to
+# something meaningful, which means we can't resolve the relative
+# submodules.  Override the remote here.
+( cd _deps/unanimity &&
+  git remote set-url origin ssh://git@bitbucket.nanofluidics.com:7999/sat/unanimity.git &&
+  git submodule update --init --remote )
 
 echo "# BUILD"
 
